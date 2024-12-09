5b0a4b2c1c7793a74685c6ba0412aac3b1c2ab49
aleep98 <zero167@outlook.com>
2024-12-09 18:53:15 -0300

    Listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+import api from '../../services/api';
+// URL da API: /movie/now_playing?api_key=0cf5142d985256ab23f020f8636ff5de
+
+
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "0cf5142d985256ab23f020f8636ff5de",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            setFilmes(response.data.results.slice(0, 10))
+        }
+
+        loadFilmes();
+    }, [])
+
+
     return(
-        <div>
-        <h1>Teste</h1>
+        <div className="container">
+            <div className="listta-filmes">
+            {filmes.map((filme) => {
+                return(
+                    <article key = {filme.id}>
+                       <strong>{filme.title}</strong>
+                       <img src={`https://image.tmdb.org/t/´/original/${filme.poster_path}`} alt={filme.title}/>
+                       <Link to ={`/fime/${filme.id}`}>Acessar</Link>     
+                    </article>
+                )
+            })}
+            </div>
         </div>
     )
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,11 @@
+import axios from "axios";
+
 // Base da URL: https://api.themoviedb.org/3/
 // URL da API: /movie/now_playing?api_key=0cf5142d985256ab23f020f8636ff5de
 
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+});
+
+export default api;
