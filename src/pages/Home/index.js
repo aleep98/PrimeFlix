a02c40b1489c25d72d1deb4cb5eb38e096238cb5
aleep98 <zero167@outlook.com>
2024-12-15 12:12:40 -0300

    Estilização

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from '../../services/api';
+import {Link} from 'react-router-dom'
+import './home.css'
 // URL da API: /movie/now_playing?api_key=0cf5142d985256ab23f020f8636ff5de
 
 
@@ -26,13 +28,13 @@ function Home(){
 
     return(
         <div className="container">
-            <div className="listta-filmes">
+            <div className="lista-filmes">
             {filmes.map((filme) => {
                 return(
                     <article key = {filme.id}>
                        <strong>{filme.title}</strong>
-                       <img src={`https://image.tmdb.org/t/´/original/${filme.poster_path}`} alt={filme.title}/>
-                       <Link to ={`/fime/${filme.id}`}>Acessar</Link>     
+                       <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                       <Link to ={`/filme/${filme.id}`}>Acessar</Link>     
                     </article>
                 )
             })}
